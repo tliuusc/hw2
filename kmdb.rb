9b0d132151d9946b2b2ed7c2a2c399dbba1b3074
Terry Liu <terry.liu@kellogg.northwestern.edu>
2023-05-03 21:49:48 +0000

    Phase 2

--- kmdb.rb
+++ kmdb.rb
@@ -238,7 +238,18 @@ puts "======"
 puts ""
 
 # Query the movies data and loop through the results to display the movies output.
+movies = Movie.all
 
+for movie in movies
+    title = movie["title"]
+    year_released = movie["year_released"]
+    rated = movie["rated"]
+    studio = Studio.find_by({"id" => movie["studio_id"]})
+    studio_name = studio["name"]
+   
+    puts "#{title} #{year_released} #{rated} #{studio_name}" 
+end
+  
 # Prints a header for the cast output
 puts ""
 puts "Top Cast"
@@ -246,4 +257,14 @@ puts "========"
 puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie.
-# TODO!
+roles = Role.all
+
+for role in roles
+    movie = Movie.find_by({"id" => role["movie_id"]})
+    movie_name = movie["title"]
+    actor = Actor.find_by({"id" => role["actor_id"]})
+    actor_name = actor["name"]
+    character_name = role["character_name"]
+   
+    puts "#{movie_name} #{actor_name} #{character_name}" 
+end
